7890ed94129a9303cde0335301d01b8418f3d4d1
JasperWi <48508627+JasperWi@users.noreply.github.com>
2024-03-23 11:44:55 +0100

    adding progress bar

--- backtranslation.py
+++ backtranslation.py
@@ -3,6 +3,7 @@ import sentencepiece as spm
 from transformers import MarianMTModel, MarianTokenizer
 from contextlib import redirect_stdout, redirect_stderr
 import io
+from tqdm import tqdm
 
 def load_squad_dataset(path: str):
     with open(path, "r") as f:
@@ -14,8 +15,16 @@ def save_squad_dataset(squad_dict, path: str):
         json.dump({"data": squad_dict}, f)
 
 def apply_back_translation(squad_dict, src_lang: str, tgt_lang: str):
+
+    # progress bar for each paragraph
+    num_para = sum([len(article["paragraphs"]) for article in squad_dict])
+    progress_bar = tqdm(total=num_para, desc="Back translating paragraphs", position=0)
+
     for article in squad_dict:
         for paragraph in article["paragraphs"]:
+            
+            progress_bar.update(1)
+
             paragraph["context"] = back_translate(paragraph["context"], src_lang, tgt_lang)
             for qa in paragraph["qas"]:
                 qa["question"] = back_translate(qa["question"], src_lang, tgt_lang)
@@ -28,11 +37,11 @@ def merge_squad_datasets(squad_dict1, squad_dict2):
 
 def back_translate(text: str, src_lang: str, tgt_lang: str):
     # Use the backtranslation API
-    model1_name = "Helsinki-NLP/opus-mt-en-" + tgt_lang
+    model1_name = "Helsinki-NLP/opus-mt-" + src_lang + "-" + tgt_lang
     tokenizer1 = MarianTokenizer.from_pretrained(model1_name)
     model1 = MarianMTModel.from_pretrained(model1_name)
 
-    model2_name = "Helsinki-NLP/opus-mt-" + tgt_lang + "-en"
+    model2_name = "Helsinki-NLP/opus-mt-" + tgt_lang + "-" + src_lang
     tokenizer2 = MarianTokenizer.from_pretrained(model2_name)
     model2 = MarianMTModel.from_pretrained(model2_name)
 
@@ -58,13 +67,13 @@ def back_translate(text: str, src_lang: str, tgt_lang: str):
 
 if __name__ == "__main__": 
     # Load the SQuAD dataset
-    squad_dataset = load_squad_dataset("train-v2.0.json")
+    squad_dataset = load_squad_dataset("dev-v2.0.json")
 
 
 
     # Apply back translation to the dataset using Google Translate into 5 different languages
-    for lang in ["fr", "de", "es", "zh", "ja"]:
-        test_string = back_translate("This is a test string to see if this works as well as it should", "en", lang)
-        print(lang, ":  ", test_string)	
-        #squad_dataset = apply_back_translation(squad_dataset, "en", lang)
-        #save_squad_dataset(squad_dataset, f"train-v2.0-with-back-translation-{lang}.json")
+    for lang in ["fr", "de"]:#, "es", "zh", "ja"]:
+        #test_string = back_translate("This is a test string to see if this works as well as it should", "en", lang)
+        #print(lang, ":  ", test_string)
+        squad_dataset_back_translated = apply_back_translation(squad_dataset, "en", lang)
+        save_squad_dataset(squad_dataset_back_translated, f"train-v2.0-with-back-translation-{lang}.json")
